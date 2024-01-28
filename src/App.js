511a619d543085f79e8ab9f9ba87873bbdd065b7
Hashidgaba <hashidgaba@gmail.com>
2024-01-28 23:28:40 +0500

    first changing in this app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          i have jsut changes in this app
         </p>
         <a
           className="App-link"
