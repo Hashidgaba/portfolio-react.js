a4b0793c0c98fac7aafeffd23862e763f0e31ea9
Hashidgaba <hashidgaba@gmail.com>
2024-02-05 00:45:13 +0500

    react poject

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,6 @@ import './Footer.css';
 export default function Footer() {
   return (
     <div className='footer-cont'>
-      <div>
-        <a href='https://www.facebook.com/hashidgaba' target='blank'><i class="fa-brands fa-facebook"></i></a>
-        <a href='/'><i class="fa-brands fa-instagram"></i></a>
-        <a href='/'><i class="fa-brands fa-snapchat"></i></a>
-        <a href='/'><i class="fa-brands fa-linkedin"></i></a>
-      </div>
-
       <p>
         &copy;copyright Hashid gaba. All rights reserved.</p>
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Header.css';
+import '../index.css';
 export default function Header() {
 
     document.addEventListener("scroll", scrollHandler);
