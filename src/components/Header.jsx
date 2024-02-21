746d43d7d4449649ab7e2e9ee5d40878ff288f39
Hashidgaba <hashidgaba@gmail.com>
2024-02-21 22:53:14 +0500

    all changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,10 +21,10 @@ export default function Header() {
         <div className='header' id='home'>
             <div className="Navbar">
                 <ul className="left">
-                    <li ><a href="/" className="link-1">Home</a></li>
-                    <li ><a to="/about" className="link-1">About Me</a></li>
-                    <li><a href="/contact" className="link-1">Contact</a></li>
-                    <li ><a href="/services" className="link-1">Services</a></li>
+                    <li ><a href="" className="link-1">Home</a></li>
+                    <li ><a href="#about" className="link-1">About Me</a></li>
+                    <li><a href="#contact" className="link-1">Contact</a></li>
+                    <li ><a href="#services" className="link-1">Services</a></li>
                 </ul>
                 <div className="right">
                     <img src='https://media-mct1-1.cdn.whatsapp.net/v/t61.24694-24/363097372_223896063481145_5608192509917987327_n.jpg?ccb=11-4&oh=01_AdRx3V-cbcK26MziLJI6LF5UCykm-R1L9cfskm--cE7Cgg&oe=65D1A53D&_nc_sid=e6ed6c&_nc_cat=110'></img>
